20f4a7407d283cd6f17d8b2b9377d3a9878c09b3
ArhanB <arhan@growth-loop.io>
2025-08-28 10:41:44 +0530

    Changed buisness unit names

--- data_loader.py
+++ data_loader.py
@@ -8,16 +8,16 @@ def load_data():
     """
     np.random.seed(42)
     months = pd.date_range("2024-08-01", periods=6, freq="MS")
-    business_units = ["North America Sales", "EMEA Sales", "Customer Support"]
+    business_units = ["Car Dept", "Mobile Dept", "Computer Dept"]
 
     rows = []
     for bu in business_units:
-        base_cost = {"North America Sales": 4_800_000,
-                     "EMEA Sales": 2_900_000,
-                     "Customer Support": 2_500_000}[bu]
-        base_rev = {"North America Sales": 12_000_000,
-                    "EMEA Sales": 7_200_000,
-                    "Customer Support": 3_500_000}[bu]
+        base_cost = {"Car Dept": 4_800_000,
+                     "Mobile Dept": 2_900_000,
+                     "Computer Dept": 2_500_000}[bu]
+        base_rev = {"Car Dept": 12_000_000,
+                    "Mobile Dept": 7_200_000,
+                    "Computer Dept": 3_500_000}[bu]
         for i, m in enumerate(months):
             labor_cost = base_cost * (1 + 0.01*i) + np.random.normal(0, 30_000)
             revenue = base_rev * (1 + 0.015*i) + np.random.normal(0, 100_000)
